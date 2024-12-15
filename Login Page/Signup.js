5559046b07cfdede810cefc264cdb4d91a8cf269
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2024-12-15 17:19:49 +0000

    asd

--- Login Page/Signup.js
+++ Login Page/Signup.js
@@ -1,32 +1,28 @@
-import {
-    createUserWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
 
 import { auth } from "../firebaseConfig.js"; // Correct import path for firebaseconfig.js
 
-
 document.getElementById("sign-up-text").addEventListener("click", async () => {
-    document.getElementById("sign-up").addEventListener("click", async (e) => {
-        e.preventDefault();
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-        const name = document.getElementById("name").value;
-        const Confpwd = document.getElementById("ConfirmPassword").value;
+  document.getElementById("sign-up").addEventListener("click", async (e) => {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const name = document.getElementById("name").value;
+    const Confpwd = document.getElementById("ConfirmPassword").value;
 
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-            window.location.replace("../Home/Home.html");
-        } catch (error) {
-            if (error.code === 'auth/email-already-in-use') {
-                alert("The email address is already in use by another account.");
-            } else {
-                alert(`Error: ${error.message}`);
-            }
-        }
-    });
-})
-
-
-
-
-
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      window.location.replace("../Home/Home.html");
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("The email address is already in use by another account.");
+      } else {
+        alert(`Error: ${error.message}`);
+      }
+    }
+  });
+});
