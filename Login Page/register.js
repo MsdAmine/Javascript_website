4195b5406ccce44afa5c10f3b19530f401f1daf0
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2024-12-15 15:55:33 +0000

    edited the register.js file

--- Login Page/register.js
+++ Login Page/register.js
@@ -1,9 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
 import {
-  getAuth,
   signInWithEmailAndPassword,
   sendPasswordResetEmail,
 } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
+
+import { auth } from "../firebaseConfig";
 
 function ResetPwd() {
   const email = document.getElementById("email").value;
@@ -24,23 +24,11 @@ function ResetPwd() {
       });
   }
 }
-
 window.ResetPwd = ResetPwd;
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD_P7F4nXxo_jJPxekO4icvheFUD0sVv68",
-  authDomain: "javascript-7aaf4.firebaseapp.com",
-  projectId: "javascript-7aaf4",
-  storageBucket: "javascript-7aaf4.firebasestorage.app",
-  messagingSenderId: "179968535199",
-  appId: "1:179968535199:web:5f035d395b12c4c044d0aa",
-  measurementId: "G-ZCZE4ZZM9Q",
-};
+const button = document.getElementById("sign-in");
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-document.getElementById("sign-in").addEventListener("click", async (e) => {
+button.addEventListener("click", async (e) => {
   e.preventDefault();
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
