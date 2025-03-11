1cb981371967286dd67dba3383b6e85506384b39
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2025-03-11 01:50:24 +0000

    Users can now change their username and email in the profile tab

--- Profile/Profile.js
+++ Profile/Profile.js
@@ -1,7 +1,102 @@
-import { auth } from "../firebaseConfig.js";
-import { signOut } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { db, auth } from "../firebaseConfig.js";
+import {
+    signOut,
+    onAuthStateChanged,
+    updateEmail,
+    updatePassword,
+    sendEmailVerification,
+    reauthenticateWithCredential,
+    EmailAuthProvider
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import {
+    collection,
+    getDocs,
+    doc,
+    setDoc
+} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
 
-const logoutBtn = document.getElementById('logout'); 
+let userId = ""
+
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+        userId = user.uid;
+
+        try {
+            const usernameSnapshot = await getDocs(collection(db, `users/${userId}/user`));
+            let username = "";
+            const userEmail = user.email;
+
+            usernameSnapshot.forEach((doc) => {
+                const userData = doc.data();
+                if (userData.username) {
+                    username = userData.username;
+                }
+            });
+
+            document.getElementById("full-name").placeholder = username;
+            document.getElementById("email").placeholder = userEmail;
+        } catch (error) {
+            console.error("Error fetching username:", error);
+        }
+    } else {
+        console.error("No user signed in.");
+        alert("You must be logged in.");
+        window.location.href = "/Login Page/Login_page.html";
+    }
+});
+
+async function usernameUpdate(Fullname) {
+
+    try {
+        const data = { username: Fullname.value.trim() };
+
+        const userRef = doc(db, `users/${userId}/user/${userId}`);
+        await setDoc(userRef, data);
+
+        console.log("Username updated successfully!");
+        
+        document.getElementById("full-name").value = "";
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+async function emailUpdate(email) {
+    const user = auth.currentUser;
+
+    try {
+        // Update the email to the new address
+        await updateEmail(user, email.value);
+        console.log("Email updated successfully!");
+
+        document.getElementById("email").value = "";
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const Fullname = document.getElementById("full-name");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    if (Fullname.value.trim() !== "") {
+        usernameUpdate(Fullname);
+    }
+
+    if (email.value.trim() !== "") {
+        emailUpdate(email)
+    }
+    if (password.value.trim() !== "") {
+
+    }
+})
+
+const logoutBtn = document.getElementById('logout');
 
 logoutBtn.addEventListener("click", () => {
     signOut(auth)
